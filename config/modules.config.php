34e04331216293a789562321f8007181b8b5cfb5
redaabdel665 <49130338+redaabdel665@users.noreply.github.com>
2021-02-25 21:11:17 +0100

    APRES INSTALL

--- config/modules.config.php
+++ config/modules.config.php
@@ -12,7 +12,15 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Laminas\Form',
+    'Laminas\I18n',
+    'Laminas\Mvc\Console',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\Hydrator',
+    'Laminas\Db',
     'Laminas\Router',
     'Laminas\Validator',
+    'Laminas\DeveloperTools',
     'Application',
 ];
